052a5d1b379ff77b1221d1ff5f6fd32d28835380
shakib2123 <mshakibalhasan21@gmail.com>
2023-10-09 01:11:00 +0600

    firebase file modified

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyA9CFq5Q4EzYcAtDwIRPtKcwzzY0OpBgcU",
-  authDomain: "event-management-12427.firebaseapp.com",
-  projectId: "event-management-12427",
-  storageBucket: "event-management-12427.appspot.com",
-  messagingSenderId: "739083839585",
-  appId: "1:739083839585:web:05325cce98de3fb17da220",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
